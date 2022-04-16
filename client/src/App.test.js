656a8e43ce57c04cd3e9e508a7f976603496fc5c
Bruno Pinho <brunopinho@outlook.com>
2022-04-15 22:48:10 -0300

    test: refactor app test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,9 @@
 import { render } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 
 describe('App', () => {
   it('should ...', () => {
-    render(<App />);
+    render(<Router><App /></Router>);
   });
 });
