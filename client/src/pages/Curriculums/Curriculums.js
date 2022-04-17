5f37429d53357aa4b05ffe6a58c7dd59f5d56ee9
Bruno Pinho <brunopinho@outlook.com>
2022-04-17 17:34:06 -0300

    fix: logic to get curriculums

--- client/src/pages/Curriculums/Curriculums.js
+++ client/src/pages/Curriculums/Curriculums.js
@@ -9,15 +9,14 @@ const Curriculums = () => {
   const [curriculums, setCurriculums] = useState([]);
 
   useEffect(() => {
-    const curriculumsList = JSON.parse(localStorage.getItem('curriculums'));
+    let curriculumsList = JSON.parse(localStorage.getItem('curriculums'));
 
     if (!curriculumsList) {
-      localStorage.setItem('curriculums', JSON.stringify(MockedCurriculums));
+      curriculumsList = localStorage.setItem('curriculums', JSON.stringify(MockedCurriculums));
+      setCurriculums(curriculumsList);
     }
 
     setCurriculums(curriculumsList);
-
-    console.log(curriculumsList);
   }, []);
 
   return (
@@ -25,21 +24,24 @@ const Curriculums = () => {
       <Header />
       <StyledCurriculums>
         <h1>Currículos</h1>
+        {console.log("AQUI", curriculums)}
         {
-          curriculums.map((curriculum) => (
-            <article key={curriculum.id} className="curriculums__card">
-              <div className="card__infos">
-                <h2 className="card__name">{curriculum.name}</h2>
-                <h3 className="card__role">{curriculum.role}</h3>
-                <ul className="card__skills">
-                  {curriculum.stacks.map((stack, index) => (<li key={index} className="card__skill">{stack}</li>))}
-                </ul>
-              </div>
-              <div className="card__link">
-                <Link to={`/curriculos/${curriculum.id}`}><img src={resume} alt="A resume illustration" height={50} />Ver</Link>
-              </div>
-            </article>
-          ))
+          curriculums
+            ? (curriculums.map((curriculum) => (
+              <article key={curriculum.id} className="curriculums__card">
+                <div className="card__infos">
+                  <h2 className="card__name">{curriculum.name}</h2>
+                  <h3 className="card__role">{curriculum.role}</h3>
+                  <ul className="card__skills">
+                    {curriculum.stacks.map((stack, index) => (<li key={index} className="card__skill">{stack}</li>))}
+                  </ul>
+                </div>
+                <div className="card__link">
+                  <Link to={`/curriculos/${curriculum.id}`}><img src={resume} alt="A resume illustration" height={50} />Ver</Link>
+                </div>
+              </article>
+            )))
+            : (<p>Carregando...</p>)
         }
         <div className="add__button--wrapper">
           <Link to="/curriculo/novo" className="add__button">Adicionar currículo</Link>
